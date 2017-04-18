7a5fd6894d7656b44fc096379dba8736db1a016f
darkenvy <darkenvy6@gmail.com>
2017-04-18 20:32:59 +0000

    added redirect to latest point

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ var db = require('./models');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', function(req, res){
+  res.status(200).send("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+});
+
+app.get('/latest', function(req, res) {
+  db.gps_tracker.findAll({
+    limit: 1,
+    order: [ [ 'createdAt', 'DESC' ]]
+  }).then(function(entries) {
+    //res.send(JSON.stringify(entries[0].lat));
+    var url = 'http://maps.google.com/maps?q=loc:';
+    url += entries[0].lat + ',';
+    url += entries[0].long;
+    res.redirect(url);
+  });
+});
+
 app.post('/', function(req, res) {
   if (!req.body['custom field 1']) {
     res.status(400).send('Missing Device Name'); 
@@ -27,4 +44,5 @@ app.post('/', function(req, res) {
   res.status(200).send('success');
 });
 
+console.log('listening on port 3000');
 app.listen(3000);
